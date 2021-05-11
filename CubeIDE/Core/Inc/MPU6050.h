6d99a4adfe215776fabede4e9ad9e2e0a8bc1a84
Carlos Lecaro <46472584+hydracrux@users.noreply.github.com>
2021-05-11 18:15:31 -0500

    Start Project on STM32CubeIDE
    - Implementation on STM32F7 board
    - Set USART3 for debug
    - Set I2C4 for MPU6050 Module
    - Add Class MPU6050

---
+++ CubeIDE/Core/Inc/MPU6050.h
@@ -0,0 +1,17 @@
+/*
+ * MPU6050.h
+ *
+ *  Created on: May 11, 2021
+ *      Author: hydra
+ */
+
+#ifndef MPU6050_H_
+#define MPU6050_H_
+
+class MPU6050
+{
+	public:
+		MPU6050 ();
+};
+
+#endif /* MPU6050_H_ */
---
+++ CubeIDE/Core/Src/MPU6050.cpp
@@ -0,0 +1,14 @@
+/*
+ * MPU6050.cpp
+ *
+ *  Created on: May 11, 2021
+ *      Author: hydra
+ */
+
+#include "MPU6050.h"
+
+MPU6050::MPU6050 () {
+	// TODO Auto-generated constructor stub
+
+}
+
